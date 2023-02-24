f85cd47a2c5c79ce2fcdb704ccfe946f8fb6bfce
emyrue <94935633+emyrue@users.noreply.github.com>
2023-02-24 15:00:09 -0700

    Search bar functional and pagination working, pagination buttons yet to be set up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   # placeholder logic to prevent an endless user session.
   # Comment out before_action :signout if you want to stay logged in
 
+  RECIPES_PER_PAGE = 1
+
   def index
-    @recipes = Recipe.all
+    @recipes_all = Recipe.all
+    @params = params.permit(:page, :query)
+    @current_page = params[:page].to_i || 1
+    @recipes = @recipes_all.where("lower(name) LIKE ?", "%#{params[:query].to_s.downcase}%").offset(@current_page).limit(RECIPES_PER_PAGE)
   end
 end
