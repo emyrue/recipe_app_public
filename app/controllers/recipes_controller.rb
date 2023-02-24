2f8c6919c44ae12a4372b60e0dd440c37db012e3
Aaron Keegan <AKeegan.Dev@gmail.com>
2023-02-24 08:02:42 -0500

    added minimagic gem and configured to allow for image resizing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-    
+
     @recipe.user_id = current_user.id
     if @recipe.save
       redirect_to '/'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,14 @@
+require 'mini_magick'
+
 class Recipe < ApplicationRecord
   include ImageUploader::Attachment(:image) # adds an `image` virtual attribute
   belongs_to :user
   validates :name, presence: true
   validates :ingredients, presence: true
   validates :directions, presence: true
+
+  def generate_thumbnail
+    image_derivatives!
+    image_url(:small)
+  end
 end
