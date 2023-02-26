ee6591c58261c6354e531a12d9923671181f3f98
Aaron Keegan <AKeegan.Dev@gmail.com>
2023-02-26 03:42:13 -0500

    removed unused method from applicationhelper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def generate_signout_navlink
-    content ''
-    if current_user
-      content = '
-      <li>
-        <%= link_to "sign out", destroy_user_session_path, data: { turbo_method: :delete }%>
-      </li>
-      '
-    end
-    content.html_safe
-  end
 end
